ade4b6f92f7be4ca466e6cb637189efacea047f5
jorgeguzman20 <jorgetazguzman@gmail.com>
2021-04-10 00:33:54 -0600

    generalField
    Campos reutilzables para el uso de ingreso de datos

--- src/views/app-views/dashboards/professional-services/index.js
+++ src/views/app-views/dashboards/professional-services/index.js
@@ -2,13 +2,14 @@ import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from 'react-router-dom';
 import Loading from 'components/shared-components/Loading';
 import generalBalance from './general-balance/';
-
+import revenue from './revenue'
 
 const ProfessionalServices = ({ match }) => {
   return(
     <Suspense fallback={<Loading cover="content"/>}>
       <Switch>
         <Route path={`${match.url}/general-balance`} component={generalBalance} />
+        <Route path={`${match.url}/revenue`} component={revenue} />
         <Redirect from={`${match.url}`} to={`${match.url}/default`} />
       </Switch>
     </Suspense>
