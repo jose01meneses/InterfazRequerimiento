f4757bf8ea3f41d3bc26e28bf3498b157b63c383
davidrojas1995 <66500205+davidrojas1995@users.noreply.github.com>
2021-04-11 19:23:55 -0600

    Blog

---
+++ src/views/app-views/dashboards/blog/faq/faqData.js
@@ -0,0 +1,61 @@
+export const faqCategories = [
+	{
+		key: "faq-1",
+		
+		title: "Introduccion",
+	},
+	{
+		key: "faq-2",
+		
+		title: "Ingresos",
+	},
+	{
+		key: "faq-3",
+		
+		title: "Egresos",
+	},
+]
+
+export const faqList = [
+	
+		{
+			id: "faq-1",
+			data: [
+				{
+					key: "faq-1-1",
+					title: "Bienvenida",
+					desc: "Gracias por utilizar esta aplicacion"
+				
+					
+					
+				}
+			]
+		},
+
+		{
+			id: "faq-2",
+			data: [
+				{
+					key: "faq-1-1",
+					title: "Como registrar un ingreso",
+					desc: "Para registrar un ingreso... "
+				}
+				
+				
+
+			]
+		},
+
+		{
+			id: "faq-3",
+			data: [
+				{
+					key: "faq-1-1",
+					title: "Como registrar un egreso",
+					desc: "Para registrar un egreso..."
+				}
+
+			]
+		}
+	
+]
